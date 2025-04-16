749864c1688641f428cc3988ff49e39caee5c451
Alex <belrev@yahoo.com>
2025-04-16 17:27:07 +0300

    Mиссия 8.4 (правильно)

--- models/events_clean.sql
+++ models/events_clean.sql
@@ -1,8 +1,14 @@
+{% set date = var("date", none) %}
+
 select distinct *,
   {{ updated_at() }}
 from {{ source('scooters_raw', 'events') }}
 {% if is_incremental() %}
+  {% if date %}
+  where date("timestamp") = date '{{ date }}'
+  {% else %}  
   where "timestamp" > (select max("timestamp") from {{ this }})
+  {% endif %}
 {% else %}
   where "timestamp" < date '2023-08-01'
 {% endif %}
