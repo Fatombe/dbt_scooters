34e3131e0c69fc37ca3f758a18d7c7582b2c4192
Alex <belrev@yahoo.com>
2025-04-23 23:40:37 +0300

    Миссия 11.1+

--- models/sex_age_pivot.sql
+++ models/sex_age_pivot.sql
@@ -1,8 +1,9 @@
 select
-  age,
-  {{ dbt_utils.pivot(
-      'sex', ["M", "F"]) }}
-from {{ ref('trips_users') }}
-
-group by age
-order by age
+    age,
+    {{dbt_utils.default__pivot("sex", ["M", "F"])}}
+from
+    {{ ref("trips_users") }}
+group by
+    1
+order by
+    1
