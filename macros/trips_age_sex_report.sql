fd7e401ef8185f996ebaf0930c374c9c357c4b10
Alex <belrev@yahoo.com>
2025-04-28 22:44:03 +0300

    Миссия 13.2

---
+++ macros/trips_age_sex_report.sql
@@ -0,0 +1,29 @@
+{% macro trips_age_sex_report(trips_table, granularity) %}
+  
+{% set time_column %}
+    {% if granularity == 'daily'  %}
+        "date"
+    {% elif granularity == 'weekly' %}
+        date_trunc('week', "date")::date as "week"
+    {% elif granularity == 'monthly' %}
+    date_trunc('month', "date")::date as "month"
+    {% else %}
+        {{ exceptions.raise_compiler_error("Invalid granularity: " ~ granularity) }}
+    {% endif %}
+{% endset %}
+
+SELECT
+    {{time_column}},
+    age,
+    coalesce(sex, 'UNKNOWN') as sex,
+    count(*) as trips,
+    sum(price_rub) as revenue_rub
+FROM {{trips_table}}
+GROUP BY
+    1,
+    2,
+    3
+{% endmacro %}
+
+
+
